9fa98bdb49a3f1653f78ee551231cc18a04f9cdc
elias <eliaseshetu2847@gmail.com>
2024-05-12 20:55:53 +0300

    dashboard

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,11 +1,19 @@
-// src/index.js
-
 import $ from 'jquery';
+import debounce from 'lodash/debounce';
+
+let count = 0;
 
 $(function() {
   $('body').append('<p>Holberton Dashboard</p>');
   $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<button>Click here to get started</button>');
+  $('body').append('<button id="clickButton">Click here to get started</button>');
   $('body').append('<p id="count"></p>');
   $('body').append('<p>Copyright - Holberton School</p>');
+
+  function updateCounter() {
+    count++;
+    $('#count').text(`${count} clicks on the button`);
+  }
+
+  $('#clickButton').on('click', debounce(updateCounter, 500));
 });
