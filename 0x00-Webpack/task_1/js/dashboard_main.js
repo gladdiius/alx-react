84bac0492f567331f0fa1d97d22ad9eb150c322c
elias <eliaseshetu2847@gmail.com>
2024-05-12 18:06:39 +0300

    task_1

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,11 @@
+// src/index.js
+
+import $ from 'jquery';
+
+$(function() {
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button>Click here to get started</button>');
+  $('body').append('<p id="count"></p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+});
