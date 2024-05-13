cac825610dc06942ad32991717ec70412d5ab8c5
elias <eliaseshetu2847@gmail.com>
2024-05-13 12:08:19 +0300

    web pack

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,49 +1,40 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development', // Set mode to 'development'
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devtool: 'inline-source-map', // Configure inline source map
-  module: {
-    rules: [
-      {
-        test: /\.css$/, // Apply style and css loader for .css files
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i, // Apply image-webpack-loader for image files
-        type: 'asset/resource',
-        use: [
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              mozjpeg: {
-                progressive: true,
-                quality: 65,
-              },
-              // Optimize PNG images, disabled by default
-              optipng: {
-                enabled: false,
-              },
-              pngquant: {
-                quality: [0.65, 0.90],
-                speed: 4,
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-              // the webp option will enable WEBP
-              webp: {
-                quality: 75,
-              },
+    mode: 'production',
+    entry: './task_2/js/dashboard_main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
             },
-          },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                    },
+                ],
+            },
         ],
-      },
-    ],
-  },
+    },
+    devtool: 'inline-source-map',
 };
