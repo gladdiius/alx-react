35aea77357d1f127e7c686f366cd9fc5264e4da2
elias <eliaseshetu2847@gmail.com>
2024-05-30 23:11:01 +0300

    6-deeply.js

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,8 +1,18 @@
-import { Map, List } from 'immutable';
+import { Map } from 'immutable';
 
+// Function to merge two objects deeply
 export default function mergeDeeplyElements(page1, page2) {
   const map1 = Map(page1);
   const map2 = Map(page2);
-  const mergedMap = map1.mergeDeep(map2);
-  return List.of(mergedMap);
+
+  // Recursively merge the two maps
+  const mergedMap = map1.mergeWith((oldVal, newVal) => {
+    if (Map.isMap(oldVal)) {
+      return oldVal.mergeWith((oldNestedVal, newNestedVal) => oldNestedVal.concat(newNestedVal),
+        newVal);
+    }
+    return newVal;
+  }, map2);
+
+  return mergedMap;
 }
