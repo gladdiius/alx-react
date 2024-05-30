1060b7f17bcecac4ac6959cb9a1b8fa6cc6ec4d0
elias <eliaseshetu2847@gmail.com>
2024-05-30 22:55:24 +0300

    5-merge.js

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,12 +1,12 @@
 import { List, Map } from 'immutable';
 
 // Function to concatenate elements of two arrays into a List
-function concatElements(page1, page2) {
+export function concatElements(page1, page2) {
   return List(page1).concat(List(page2));
 }
 
 // Function to merge elements of two objects into a List
-function mergeElements(page1, page2) {
+export function mergeElements(page1, page2) {
   const map1 = Map(page1);
   const map2 = Map(page2);
 
