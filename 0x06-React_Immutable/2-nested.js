9ce1136612f697c2e9c83fc35f61893fdfaa6db1
elias <eliaseshetu2847@gmail.com>
2024-05-30 22:25:51 +0300

    2-nested

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,11 +1,4 @@
 export default function accessImmutableObject(object, array) {
-  let result = object;
-  for (const key of array) {
-    if (result[key] !== undefined) {
-      result = result[key];
-    } else {
-      return undefined;
-    }
-  }
-  return result;
+  return array.reduce((result, key) => ((result && result[key] !== undefined)
+    ? result[key] : undefined), object);
 }
