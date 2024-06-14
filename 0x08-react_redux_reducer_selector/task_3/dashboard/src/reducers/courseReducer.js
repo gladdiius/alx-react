4de4985c9900916c7bf2048c322d64c54c3dd31c
elias <eliaseshetu2847@gmail.com>
2024-06-14 12:08:49 +0300

    task 2

---
+++ 0x08-react_redux_reducer_selector/task_3/dashboard/src/reducers/courseReducer.js
@@ -0,0 +1,32 @@
+// task_4/src/reducers/courseReducer.js
+
+const initialState = [];
+
+const courseReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_COURSE_SUCCESS:
+      return action.data.map(course => ({
+        ...course,
+        isSelected: false
+      }));
+
+    case SELECT_COURSE:
+      return state.map(course =>
+        course.id === action.index
+          ? { ...course, isSelected: true }
+          : course
+      );
+
+    case UNSELECT_COURSE:
+      return state.map(course =>
+        course.id === action.index
+          ? { ...course, isSelected: false }
+          : course
+      );
+
+    default:
+      return state;
+  }
+};
+
+export default courseReducer;
---
+++ 0x08-react_redux_reducer_selector/task_3/dashboard/src/reducers/courseReducer.test.js
@@ -0,0 +1,68 @@
+// task_4/src/reducers/courseReducer.test.js
+
+import courseReducer from './courseReducer';
+import {
+  FETCH_COURSE_SUCCESS,
+  SELECT_COURSE,
+  UNSELECT_COURSE
+} from '../actions/courseActionTypes';
+
+// Test suite for courseReducer
+describe('courseReducer', () => {
+  // Test default state returns an empty array
+  it('should return the initial state with no action', () => {
+    expect(courseReducer(undefined, {})).toEqual([]);
+  });
+
+  // Test FETCH_COURSE_SUCCESS action
+  it('should handle FETCH_COURSE_SUCCESS action', () => {
+    const action = {
+      type: FETCH_COURSE_SUCCESS,
+      data: [
+        { id: 1, name: "ES6", credit: 60 },
+        { id: 2, name: "Webpack", credit: 20 },
+        { id: 3, name: "React", credit: 40 }
+      ]
+    };
+    const expectedState = [
+      { id: 1, name: "ES6", credit: 60, isSelected: false },
+      { id: 2, name: "Webpack", credit: 20, isSelected: false },
+      { id: 3, name: "React", credit: 40, isSelected: false }
+    ];
+    expect(courseReducer(undefined, action)).toEqual(expectedState);
+  });
+
+  // Test SELECT_COURSE action
+  it('should handle SELECT_COURSE action', () => {
+    const initialState = [
+      { id: 1, name: "ES6", credit: 60, isSelected: false },
+      { id: 2, name: "Webpack", credit: 20, isSelected: false },
+      { id: 3, name: "React", credit: 40, isSelected: false }
+    ];
+    const action = { type: SELECT_COURSE, index: 2 };
+    const expectedState = [
+      { id: 1, name: "ES6", credit: 60, isSelected: false },
+      { id: 2, name: "Webpack", credit: 20, isSelected: true },
+      { id: 3, name: "React", credit: 40, isSelected: false }
+    ];
+    expect(courseReducer(initialState, action)).toEqual(expectedState);
+  });
+
+  // Test UNSELECT_COURSE action
+  it('should handle UNSELECT_COURSE action', () => {
+    const initialState = [
+      { id: 1, name: "ES6", credit: 60, isSelected: false },
+      { id: 2, name: "Webpack", credit: 20, isSelected: true },
+      { id: 3, name: "React", credit: 40, isSelected: false }
+    ];
+    const action = { type: UNSELECT_COURSE, index: 2 };
+    const expectedState = [
+      { id: 1, name: "ES6", credit: 60, isSelected: false },
+      { id: 2, name: "Webpack", credit: 20, isSelected: false },
+      { id: 3, name: "React", credit: 40, isSelected: false }
+    ];
+    expect(courseReducer(initialState, action)).toEqual(expectedState);
+  });
+
+  // Additional tests can be added as needed
+});
